eac7503bcf14835198405b3406069117483cf0ad
Orli Feuchtwang <orli.feuchtwang@gmail.com>
2018-01-21 02:03:11 -0800

    icons and agreements

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -22,21 +22,21 @@ Template.body.helpers({
 
 Template.body.events({
   'click .nav-button'(e) {
- 	
- 	document.getElementById("main").style.display = 'none';
 
  	var id = e.target.parentElement.id;
     
     if(id == "agreement") {
-    	document.getElementById("agreement-page").style.display = 'block';
+    	document.getElementById("agreement-page").classList.remove("page");
     } else if(id == "shopping-list") {
 
 
     } else if(id == "tasks") {
-    	document.getElementById("task-page").style.display = 'block';
+    	document.getElementById("agreement-page").classList.remove("page");
     } else {
 
     }
+
+    document.getElementById("main").style.display = 'none';
   },
 });
 
@@ -54,7 +54,7 @@ Template.agreementPage.events({
 	},
 
 	'click #back'(e) {
-		document.getElementById("agreement-page").style.display = 'none';
+		document.getElementById("agreement-page").classList.add("page");
 		document.getElementById("main").style.display = 'block';
 	},
 
@@ -105,7 +105,6 @@ Template.agreement.events({
 	'click #accept-icon'(e) {
 
 	 	var idString = e.target.parentElement.parentElement.id;
-	 	idString = idString.slice(10,idString.indexOf("\")"));
 
 	 	agreedMembers = Template.currentData().agreedMembers;
 	 	agreedMembers.push(String(Meteor.userId()));
@@ -114,22 +113,27 @@ Template.agreement.events({
 	 	var id;
 
 	 	for (var i = agreements.length - 1; i >= 0; i--) {
-	 		if(String(agreements[i]._id._str) == idString) {
+
+	 		if(agreements[i]._id == idString) {
 	 			id = agreements[i]._id;
 	 		}
 	 	}
+
+	 
+	 	console.log(Agreements.find({}).fetch());
 
 	 	Agreements.update(id, {
 	      $set: { agreedMembers: agreedMembers },
 	    });
 
-	 	console.log(agreedMembers.length);
-	 	console.log(Users.find().fetch().length);
+	 	console.log(Agreements.find({}).fetch());
 
 	    if(agreedMembers.length == Users.find().fetch().length) {
 	 		Agreements.update(id, {
 		      $set: { approved: true },
 		    });
+
+		    console.log(Agreements.find({}).fetch());
 	 	}
 
 	 },
